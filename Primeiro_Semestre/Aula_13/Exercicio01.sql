e9438cb6d90c8476e76ca368e59883bdc75ba89a
Murillo Ferreira <murilloferreira348@gmail.com>
2024-10-31 17:29:44 -0300

    Testando algumas coisas

--- Primeiro_Semestre/Aula_13/Exercicio01.sql
+++ Primeiro_Semestre/Aula_13/Exercicio01.sql
@@ -1,20 +1,19 @@
 /*
-Questão 1: Procedure para Listar Pedidos de um Cliente 
-Descrição: Crie uma procedure chamada listar_pedidos_cliente que recebe o código de um cliente e exibe seus pedidos e valores. Se o cliente não existir, lance uma exceção personalizada. Use um cursor para listar os pedidos e um IF para verificar se há registros antes de exibir os pedidos.
+QuestÃ£o 1: Procedure para Listar Pedidos de um Cliente 
+DescriÃ§Ã£o: Crie uma procedure chamada listar_pedidos_cliente que recebe o cÃ³digo de um cliente e exibe seus pedidos e valores. Se o cliente nÃ£o existir, lance uma exceÃ§Ã£o personalizada. Use um cursor para listar os pedidos e um IF para verificar se hÃ¡ registros antes de exibir os pedidos.
 Requisitos:
-• Exceção: Lance uma exceção personalizada se o cliente não for encontrado.
-• JOIN: Use INNER JOIN para combinar pedido com historico_pedido.
-• IF: Verifique se o cliente possui pedidos.
-• Cursor: Utilize um cursor para iterar sobre os pedidos.
+â€¢ ExceÃ§Ã£o: Lance uma exceÃ§Ã£o personalizada se o cliente nÃ£o for encontrado.
+â€¢ JOIN: Use INNER JOIN para combinar pedido com historico_pedido.
+â€¢ IF: Verifique se o cliente possui pedidos.
+â€¢ Cursor: Utilize um cursor para iterar sobre os pedidos.
 */
 
 SET SERVEROUTPUT ON;
 
 CREATE OR REPLACE PROCEDURE listar_pedido_cliente(P_CLIENTE NUMBER) IS
-    -- Declaração de uma exceção personalizada
     MINHA_EXC EXCEPTION;
-    
-    -- Declaração do cursor para listar os pedidos
+    CLIENTE_NAO_ENCONTRADO BOOLEAN := TRUE;
+
     CURSOR HIST_PEDIDO IS 
         SELECT
             a.cod_cliente,
@@ -26,15 +25,21 @@ CREATE OR REPLACE PROCEDURE listar_pedido_cliente(P_CLIENTE NUMBER) IS
             INNER JOIN historico_pedido b ON (a.cod_pedido = b.cod_pedido)
             INNER JOIN cliente c ON (a.cod_cliente = c.cod_cliente)
         WHERE 
-            a.cod_cliente = P_CLIENTE;  -- Filtra pelo código do cliente
+            a.cod_cliente = P_CLIENTE;
 
 BEGIN
-        FOR X IN HIST_PEDIDO LOOP
-            -- Exibe o código do cliente
-            DBMS_OUTPUT.PUT_LINE(x.cod_cliente);
-        END LOOP;
-    EXCEPTION
-        WHEN no_data_found THEN
-        dbms_output.put_line('NAO EXISTE CLIENTE CADASTRADO');
+    FOR X IN HIST_PEDIDO LOOP
+        CLIENTE_NAO_ENCONTRADO := FALSE;
+        DBMS_OUTPUT.PUT_LINE('CÃ³digo do Cliente: ' || x.cod_cliente || ' - Nome: ' || x.nom_cliente || ' - Valor Pedido: ' || x.val_total_pedido);
+    END LOOP;
+
+    IF CLIENTE_NAO_ENCONTRADO THEN
+        RAISE MINHA_EXC;
+    END IF;
+
+EXCEPTION
+    WHEN MINHA_EXC THEN
+        DBMS_OUTPUT.PUT_LINE('NAO EXISTE CLIENTE CADASTRADO');
 END;
-CALL BUSTAR_PEDIDO_CLIENTE(12);
+
+CALL listar_pedido_cliente(22); --Exemplo cliente Vinicius Castro 
