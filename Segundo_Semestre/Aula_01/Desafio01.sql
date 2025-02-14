aa6e1a0d724a689b3bdb28259ddecdfdfd4489d6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-14 11:48:47 -0300

    Atualizando da aula do dia 14

---
+++ Segundo_Semestre/Aula_01/Desafio01.sql
@@ -0,0 +1,11 @@
+-- ============================
+-- 1) Crie uma trigger para registrar as alterações de DATA DE ENTREGA  na tabela PEDIDO.
+-- ============================
+
+-- ============================
+-- 2) Crie uma trigger que consulta a  quantidade de itens na tabela ITEM_PEDIDO  e some o total de produto do mesmo pedido , caso esse total seja maior que 20 itens aplique um desconto automático que 20% .
+-- ============================
+
+-- ============================
+-- 3) Crie uma trigger em na tabela de clientes para validar se CPF/CNPJ contenha somente valores numéricos.
+-- ============================
